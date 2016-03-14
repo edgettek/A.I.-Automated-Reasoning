4e2c032fc6ca25ec759375b2dec2109af0207ef6
Kyle Edgette <KEdgette1@dhcp-10-5-5-225.wireless.rochester.edu>
2016-03-14 19:00:12 -0400

    Adding cnf

---
+++ HornClauses.java
@@ -0,0 +1,52 @@
+import propositionalLogic.*;
+
+
+public class HornClauses extends KB{
+	
+	public HornClauses() {
+		
+		super();
+		
+		Symbol myth = intern("mythical");
+		Symbol immortal = intern("immortal");
+		Symbol mammal = intern("mammal");
+		Symbol horned = intern("horned");
+		Symbol magical = intern("magical");
+		
+		add(new Implication(myth, immortal));
+		
+		add(new Implication(new Negation(myth), new Conjunction(new Negation(immortal), mammal)));
+		
+		add(new Implication(new Disjunction(immortal, mammal), horned));
+		
+		add(new Implication(horned, magical));
+		
+		
+		
+	}
+	
+	public void solveHornClauses() {
+		
+		Sentence s = new Symbol("mythical");
+		
+		System.out.println("Can we prove that the unicorn is mythical?");
+		
+		System.out.println(Test.TTEntails(this, s));
+		
+		System.out.println("Can we prove that the unicorn is magical?");
+		
+		s = new Symbol("magical");
+		
+		System.out.println(Test.TTEntails(this, s));
+		
+		System.out.println("Can we prove that the unicorn is horned?");
+		
+		s = new Symbol("horned");
+		
+		System.out.println(Test.TTEntails(this, s));
+		
+		
+		
+	}
+
+}
